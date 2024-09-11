04ad5b968bb13c06a58b8123bb4b5f3f6db5d13e
rampj48 <quattro-hyper@yandex.ru>
2024-09-11 18:11:31 +0300

    pw_1&2

---
+++ pw_1/pw_1.py
@@ -0,0 +1,6 @@
+name_surname = input("Ваши фамилия, имя?")
+age = input("Сколько Вам лет?")
+liv_place = input("Где вы живете?")
+print("Ваши фамилия, имя", name_surname)
+print("Ваш возраст", age)
+print("Вы живете в", name_surname)
