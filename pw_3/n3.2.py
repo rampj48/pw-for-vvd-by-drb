239b3a6e8a33bd15035db8c1ec2b8441bb3b6f79
rampj48 <quattro-hyper@yandex.ru>
2024-09-22 19:55:43 +0300

    completed numbers

---
+++ pw_3/n3.2.py
@@ -0,0 +1,5 @@
+number = int(input())
+if number % 2 == 0:
+    print('Число чётное')
+else:
+    print('Число не чётное')
---
+++ pw_3/n3.3.py
@@ -0,0 +1,10 @@
+number = input()
+even = []
+odd = []
+for i in number:
+    if int(i) % 2 == 0:
+        even.append(i)
+    else:
+        odd.append(i)
+print('Четные цифры - ', ', '.join(even))
+print('Нечетные цифры - ', ', '.join(odd))
